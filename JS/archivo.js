98056a345a42e73a3ee2ac62080f89c710059b26
Brenda Sliz <brenda_sliz@yahoo.com.ar>
2023-03-09 04:07:30 -0300

    se agrega js

---
+++ JS/archivo.js
@@ -0,0 +1,7 @@
+document.getElementById("boton").onclick=function(){
+    console.log("Capturamos el evento click");
+    document.getElementById("demo").innerHTML= "Dr. Leonardo G. Gagliardi, Investigador Adjunto, CONICET. Director de Tesis de Grado. Cel: (0221)155310755. e-mail: leogagliardi@quimica.unlp.edu.ar";
+}
+/*document.addEventListener('click',function(){
+
+})*/
